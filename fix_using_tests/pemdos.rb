f49ab13c06e5ba9364e1145534192fc8f2479872
Martin Di Vincenzo <mdivincenz@hotmail.com>
2020-05-15 03:59:29 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,12 +1,12 @@
 # don't forget to add: require 'pry'
 require 'pry'
 def snake_it_up(string)
-   
+ binding.pry  
   if string[0] === "s"
    string = "ssssssssss" + string
    return string
   else
     string
   end
- 
+ binding.pry
 end
